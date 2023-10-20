babf5c5b3bb87d00d3aee612042ff3f5dc89890e
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 10:52:14 +0800

    Feat: Created product list, context product context provider & wraped app with the product context

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ProductsContextProvider from './context/products-context.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ProductsContextProvider>
+        <App />
+      </ProductsContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
