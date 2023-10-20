60dd4aefbe336199ef3eae821b7b74f28b47f401
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 13:36:22 +0800

    Feat: Added route for single product

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import NotFound from './components/NotFound'
 import Shop from './components/pages/shop/Shop'
+import SingleProduct from './components/Single-product/SingleProduct'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
         <Route path='/shop' element={<Shop/>}/>
+        <Route path='/product/:id' element={<SingleProduct/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
