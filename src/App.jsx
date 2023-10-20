ea849acbf8c1a5eb3bedc80e60212149b7a8ccc9
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 11:46:19 +0800

    Feat: Not found component and route implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss'
 import HomePage from './components/HomePage'
+import NotFound from './components/NotFound'
 import { Routes, Route } from 'react-router-dom'
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
   )
