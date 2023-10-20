4ab89d968cf8f2f6a3152b72bfae1a7b9cda144c
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 15:27:36 +0800

    Feat: Cart-icon actually displaying cart items from cart context

--- src/components/cart-icon/Cart-icon.jsx
+++ src/components/cart-icon/Cart-icon.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import shoppingBag from '../../assets/cart.png'
 import './Cart-icon.styles.scss'
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
 function CartIcon() {
+  const { itemCount } = useContext(CartContext)
+
   return (
     <div className='cart-container'>
       <img src={shoppingBag} alt='shopping-cart-icon' />
-      <span className='cart-count'> 5 </span>
+      {itemCount > 0 ? <span className='cart-count'> {itemCount} </span> : null}
     </div>
   )
 }
