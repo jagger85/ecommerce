a032ed66b0f26c00e45d56048ccc5ce09961b287
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 15:28:43 +0800

    Feat: App wrapped with cart context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import ProductsContextProvider from './context/ProductsContext.jsx'
+import CartContextProvider from './context/CartContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductsContextProvider>
-        <App />
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
       </ProductsContextProvider>
     </BrowserRouter>
   </React.StrictMode>
