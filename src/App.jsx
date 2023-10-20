8d6812b040274a5b309ff9f7cd1f04dec61ecb18
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 12:07:49 +0800

    Feat: Shop component and styles implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.scss'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import NotFound from './components/NotFound'
-import { Routes, Route } from 'react-router-dom'
-
+import Shop from './components/pages/shop/Shop'
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
+        <Route path='/shop' element={<Shop/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
