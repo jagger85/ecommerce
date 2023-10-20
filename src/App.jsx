0b3c8fff8de8793f545b3f2646923eb3b5aaa5e7
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 10:34:40 +0800

    Feat: Main section added and aplied styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import MainSection from './components/Main-section/MainSection'
 function App() {
 
   return (
     <>
     <Header/>
     <Hero/>
+    <MainSection/>
     </>
   )
 }
