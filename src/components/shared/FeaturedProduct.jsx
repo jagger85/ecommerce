1062f5217f9d8bea91011edb26472b066cdd86cc
Jagger85 <andresfonsortega@yahoo.es>
2023-10-20 17:01:13 +0800

    Feat: Added logic to add item to cart

--- src/components/shared/FeaturedProduct.jsx
+++ src/components/shared/FeaturedProduct.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { isInCart } from '../helpers'
+import { CartContext } from '../../context/CartContext'
 import { useNavigate } from 'react-router-dom'
 import './FeaturedProduct.style.scss'
 
 function FeaturedProduct(product) {
   const { title, imageUrl, price, id } = product
   const navigate = useNavigate()
+  const { addProduct, cartItems } = useContext(CartContext)
   return (
     <div className='featured-product'>
       <div className='featured-image' onClick={()=> navigate(`/product/${id}`)}>
@@ -13,7 +16,11 @@ function FeaturedProduct(product) {
       <div className='name-price'>
         <h3>{title}</h3>
         <p>$ {price}</p>
-        <button className='button is-black nomad-btn'>ADD TO CART</button>
+        {
+        !isInCart(product,cartItems) 
+        ? <button className='button is-black nomad-btn' onClick={()=> addProduct(product)}>ADD TO CART</button>
+        : <button className='button is-black nomad-btn' onClick={()=>{}} id='btn-white-outline'>ADD MORE</button>
+        }
       </div>
     </div>
   )
