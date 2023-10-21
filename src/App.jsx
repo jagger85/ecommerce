023198102ec5c88cd246633b246ef31b754634c8
Jagger85 <andresfonsortega@yahoo.es>
2023-10-21 15:28:23 +0800

    Feat: Added cart route

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import HomePage from './components/HomePage'
 import NotFound from './components/NotFound'
 import Shop from './components/pages/shop/Shop'
 import SingleProduct from './components/Single-product/SingleProduct'
+import CartPage from './components/pages/Cart-page/CartPage'
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route exact path='/' element={<HomePage/>}/>
-        <Route path='/shop' element={<Shop/>}/>
-        <Route path='/product/:id' element={<SingleProduct/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/product/:id' element={<SingleProduct />} />
+        <Route path='*' element={<NotFound />} />
+        <Route path={'/cart'} element={<CartPage />} />
       </Routes>
     </div>
   )
