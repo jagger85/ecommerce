d5fbc17ee107e1cbc7cbd5d762fd0908be0ab762
Jagger85 <andresfonsortega@yahoo.es>
2023-10-24 21:21:50 +0800

    Feat: fetch from api function created

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,3 +1,17 @@
-export function isInCart (product, cartItems) {
-  return cartItems.find(item => item.id === product.id)
+export function isInCart(product, cartItems) {
+  return cartItems.find((item) => item.id === product.id)
+}
+
+const API = 'http://localhost:8080'
+
+export async function fetchFromAPI(endpoint, opts) {
+  const { method, body } = { method: 'POST', body: null, ...opts }
+  const res = await fetch(`${API}/${endpoint}`, {
+    method,
+    ...body(body && { body: JSON.stringify(body) }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  return res.json()
 }
