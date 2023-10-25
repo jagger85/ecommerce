dfcf82f33d47aa58998f6b3ba6719a3f3343de64
Jagger85 <andresfonsortega@yahoo.es>
2023-10-25 12:17:11 +0800

    Feat: Changed Api

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -2,13 +2,13 @@ export function isInCart(product, cartItems) {
   return cartItems.find((item) => item.id === product.id)
 }
 
-const API = 'http://localhost:8080'
+const API = 'https://stripe-store-invp.onrender.com'
 
 export async function fetchFromAPI(endpoint, opts) {
   const { method, body } = { method: 'POST', body: null, ...opts }
   const res = await fetch(`${API}/${endpoint}`, {
     method,
-    ...body(body && { body: JSON.stringify(body) }),
+    ...(body && { body: JSON.stringify(body) }),
     headers: {
       'Content-Type': 'application/json',
     },
