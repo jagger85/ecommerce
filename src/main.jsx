d643fa3b1408b06005a26b1dbc5e68521bad0f62
Jagger85 <andresfonsortega@yahoo.es>
2023-10-25 11:51:10 +0800

    Feat: App wrapped on stripe elements

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import ProductsContextProvider from './context/ProductsContext.jsx'
 import CartContextProvider from './context/CartContext.jsx'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductsContextProvider>
         <CartContextProvider>
+          <Elements stripe={stripePromise}>
           <App />
+          </Elements>
         </CartContextProvider>
       </ProductsContextProvider>
     </BrowserRouter>
