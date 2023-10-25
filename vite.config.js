cd33a1a3705f5bdc218ee76745392a143d1d749e
Jagger85 <andresfonsortega@yahoo.es>
2023-10-25 11:49:10 +0800

    Feat: Added support for procces.env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+  //   server: {
+  //      headers: {
+  //        "Content-Security-Policy" : [
+  //         //   "img-src  self https://i.ibb.co https://*.stripe.com",
+  //         //   "connect-src self https://api.stripe.com https://maps.googleapis.com",
+  //         //   "frame-src self https://js.stripe.com https://hooks.stripe.com",
+  //        //"script-src 'unsafe-eval' 'unsafe-inline' https://js.stripe.com 'self' "
+  //        //  https://js.stripe.com https://maps.googleapis.com",
+        
+  //      ],
+  //     }
+  //  }
+  }
 })
