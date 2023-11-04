a44ea86469ea3e17db8d64bdb3bdb80e0e15c900
Jagger85 <andresfonsortega@yahoo.es>
2023-11-04 13:18:49 +0800

    Feat: Imported payment intent and implemented route

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 require('dotenv').config({path: './.env'})
 const createCheckoutSession = require('./api/checkout')
 const webhook = require('./api/webhook')
+const paymentIntent = require('./api/paymentIntent')
+
 const app = express()
 const port = 8080
 
@@ -13,6 +15,8 @@ app.use(express.json({
 app.use(cors({origin: true}))
 
 app.get('/',( req, res) => res.send('Hello world!!!'))
+
+app.post('/create-payment-intent', paymentIntent)
 
 app.post('/create-checkout-session', createCheckoutSession)
 
