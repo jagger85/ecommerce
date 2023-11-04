b3c9613123029d7cc80daf5e476ad935417b0ca7
Jagger85 <andresfonsortega@yahoo.es>
2023-11-04 12:53:40 +0800

    Feat: Added eslint configuration and support

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': env
     },
-    plugins: [react()],
+    plugins: [react(),eslintPlugin()],
   //   server: {
   //      headers: {
   //        "Content-Security-Policy" : [
