7268ece618a2fd0d64fe2021653142fc082b75ca
Jagger85 <andresfonsortega@yahoo.es>
2023-11-04 16:00:17 +0800

    Feat: File modified to accept custom checkout

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,11 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CartContext } from '../../context/CartContext'
 import Layout from '../shared/Layout'
-import StripeCheckout from './stripe-checkout/StripeCheckout'
+import ShippingAddress from './custom-checkout/ShippingAddress'
+import CustomCheckout from './custom-checkout/CustomCheckout'
+
 import './Checkout.style.scss'
 
 function Checkout() {
-  const { itemCount, total } = useContext(CartContext)
+  const { itemCount, total, cartItems } = useContext(CartContext)
+  const [ shipping, setShipping ] = useState(null)
+  const addressShown = {
+    display: (shipping ? 'none' : 'block')
+  }
+  const cardShown = {
+    display : (shipping ? 'block' : 'none')
+  }
 
   return (
     <Layout>
@@ -13,10 +22,15 @@ function Checkout() {
         <h2>Checkout Summary</h2>
         <h3>{`Total Items: ${itemCount}`}</h3>
         <h3>{`Amount to Pay: $${total}`}</h3>
-        <StripeCheckout/>
+        <div style={addressShown}>
+          <ShippingAddress setShipping={setShipping} />
+        </div>
+        <div style={cardShown}>
+          <CustomCheckout {...{shipping, cartItems}}/>
+        </div>
       </div>
     </Layout>
   )
 }
 
-export default Checkout;
+export default Checkout
