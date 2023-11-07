9c0e90cc59468a30898bd5ea0cc137c06e8fb730
Jagger85 <andresfonsortega@yahoo.es>
2023-11-07 21:16:56 +0800

    Feat: added a console.log

--- auth/decodeJWT.js
+++ auth/decodeJWT.js
@@ -3,7 +3,7 @@ const firebase = require('../firebase')
 async function decodeJWT(req, res, next) {
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
     const idToken = req.headers.authorization.split('Bearer')[1]
-
+    console.log('----------------> idToken:  ' +idToken)
     try{
       const decodedToken = await firebase.auth.verifyIdToken(idToken)
       req['currentUser'] = decodedToken
