fd0ee6df1ed818783e2f07025f722343bd8dc8bc
Jagger85 <andresfonsortega@yahoo.es>
2023-11-07 20:07:47 +0800

    Feat: added needed endpoints

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ require('dotenv').config({path: './.env'})
 const createCheckoutSession = require('./api/checkout')
 const webhook = require('./api/webhook')
 const paymentIntent = require('./api/paymentIntent')
-
+const decodeJWT = require('./auth/decodeJWT')
+const setupIntent = require('./api/setupIntent')
+const validateUser = require('./auth/validateUser')
+const getCards = require('./api/getPaymentMethod')
+const updatePaymentIntent = require('./api/updatePaymentIntent')
 const app = express()
 const port = 8080
 
@@ -14,11 +18,19 @@ app.use(express.json({
 
 app.use(cors({origin: true}))
 
+app.use(decodeJWT)
+
 app.get('/',( req, res) => res.send('Hello world!!!'))
 
 app.post('/create-payment-intent', paymentIntent)
 
 app.post('/create-checkout-session', createCheckoutSession)
+
+app.post('/save-payment-method', validateUser, setupIntent)
+
+app.get('/get-payment-methods', validateUser, getCards )
+
+app.put('/update-payment-intent', validateUser, updatePaymentIntent)
 
 app.post('/webhook', webhook)
 
