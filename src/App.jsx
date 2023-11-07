2e37bfcf9ec79d1ed7e098666cd4af2ef2e3f5e8
Jagger85 <andresfonsortega@yahoo.es>
2023-11-07 17:31:12 +0800

    Feat: Added signIn and signUp routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import CartPage from './components/pages/Cart-page/CartPage'
 import Checkout from './components/checkout/Checkout'
 import Success from './components/checkout/stripe-checkout/Success'
 import Canceled from './components/checkout/stripe-checkout/Canceled'
-
+import SignUp from './components/sing-up/SignUp'
+import SignIn from './components/sign-in/SignIn'
 function App() {
   return (
     <div className='App'>
@@ -20,8 +21,10 @@ function App() {
         <Route path='/cart' element={<CartPage />} />
         <Route path='/checkout' element={<Checkout />} />
         <Route path='*' element={<NotFound />} />
-        <Route path='/success' element={<Success/>} />
-        <Route path='/cancelled' element={<Canceled/>} />
+        <Route path='/success' element={<Success />} />
+        <Route path='/cancelled' element={<Canceled />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Routes>
     </div>
   )
