35da8ee3aae5959e96fd5b44804b0db635d249f3
Jagger85 <andresfonsortega@yahoo.es>
2023-11-07 17:32:02 +0800

    Feat: Added firebase functionality to fetchfromApi

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,3 +1,5 @@
+import { auth } from "../firebase"
+
 export function isInCart(product, cartItems) {
   return cartItems.find((item) => item.id === product.id)
 }
@@ -6,12 +8,23 @@ const API = 'https://stripe-store-invp.onrender.com'
 
 export async function fetchFromAPI(endpoint, opts) {
   const { method, body } = { method: 'POST', body: null, ...opts }
+  const user = auth.currentUser
+  const token = user && (await user.getIdToken())
   const res = await fetch(`${API}/${endpoint}`, {
     method,
     ...(body && { body: JSON.stringify(body) }),
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
     },
   })
+
+  if (res.status === 200){
+    return res.json()
+  }else {
+    throw new Error(res.statusText)
+  }
+
+
   return res.json()
 }
