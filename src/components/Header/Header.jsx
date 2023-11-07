6c1d3b46e242e2efbce72f745570398e0118ecef
Jagger85 <andresfonsortega@yahoo.es>
2023-11-07 17:30:25 +0800

    Feat: Added signIn and signUp components to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.styles.scss'
 import CartIcon from '../cart-icon/CartIcon'
-
+import { auth } from '../../firebase'
+import { UserContext } from '../../context/UserContext'
 function Header() {
+
+  const { user } = useContext(UserContext)
+  console.log('user', user)
   return (
     <nav className='nav-menu container'>
       <div className='logo'>
@@ -16,6 +20,14 @@ function Header() {
         <li>
           <Link to='/shop'>Shop</Link>
         </li>
+        {
+          !user 
+          ? <span>
+            <li><Link to='/sign-in'> Sign In</Link></li>
+            <li><Link to='/sign-up'> Sign Up</Link></li>
+            </span>
+          : <li><Link onClick={()=> auth.signOut()}> Sign Out</Link></li>
+        }
       </ul>
       <CartIcon/>
     </nav>
