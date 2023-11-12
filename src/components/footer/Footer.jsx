29549978944101bef5d0ae0dbe2394b80c2773ab
Jagger85 <andresfonsortega@yahoo.es>
2023-11-12 19:17:10 +0800

    Feat: Updated footer text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.style.scss'
 function Footer() {
   const year = new Date().getFullYear()
 
-  return <div className='footer'>{year} NOMAD Store</div>
+  return <div className='footer'>{year} JAGGER Store</div>
 }
 
 export default Footer
